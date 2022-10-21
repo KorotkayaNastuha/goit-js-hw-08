9c4d15a926e02c72eed80c13d666feeeb91f4899
KorotkayaNastuha <skiteikina11@gmail.com>
2022-10-21 08:42:11 +0300

    01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
 
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items';
 console.log(galleryItems);
+
+
+const galleryEl = document.querySelector('.gallery');
+const markUp = galleryItems.map((image) => 
+	`<div class="gallery__item">
+		<a class="gallery__link" href=${image.original}>
+		<img class = "gallery__image"
+			src=${image.preview}
+			data-source=${image.original}
+			alt=${image.description}
+		/>
+		</a>
+	</div >`).join("");
+
+galleryEl.innerHTML = markUp;
+    
+let galleryLightbox = new SimpleLightbox('.gallery a', {captionsData: "alt",  captionDelay: 250,});
