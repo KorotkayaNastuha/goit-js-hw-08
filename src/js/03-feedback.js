4c4e2f7135bf049acf87c677ccbe55f23aadc767
KorotkayaNastuha <skiteikina11@gmail.com>
2022-10-22 20:00:20 +0300

    03-feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,24 @@
+import throttle from 'lodash.throttle';
+const formRef = document.querySelector('.feedback-form');
+const emailRef = document.querySelector('input');
+const messageRef = document.querySelector('textarea');
+const STORAGE_KEY = 'feedback-form-state';
 
+formRef.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log({email: emailRef.value, message: messageRef.value});
+    event.currentTarget.reset();
+})
+formRef.addEventListener("input", throttle(event => {
+    const formSave = { email: emailRef.value, message: messageRef.value };
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formSave));
+}, 500)
+);
+function populateTextarea() {
+  const saveMessage = localStorage.getItem(STORAGE_KEY);
+  if (saveMessage) {
+    const currentData = JSON.parse(saveMessage);
+    emailRef.value = currentData.email;
+    messageRef.value = currentData.message;
+}
+};
